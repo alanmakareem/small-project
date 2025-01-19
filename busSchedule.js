0728eb84433bb9844e084a7f5b563e848540e0b0
alanmakareem <fadhlanalan@proton.me>
2025-01-19 19:24:44 +0100

    Update busSchedule.js

--- busSchedule.js
+++ busSchedule.js
@@ -1,5 +1,6 @@
-const busNum = 125;
-const inputTime = `9:12`;
+const busNum = 136//parseInt(prompt(`Type bus number:`));
+const inputTime = `11:22`//prompt(`When do you want to go? (use H:MM format)`);
+const tt = `time`//prompt(`Type 'TIME' to show the next 10 schedule for bus no.${busNum}. Skip to see the next bus arrival time.`).toLowerCase();
 
 let startHour = null;
 let startMinute = null;
@@ -9,7 +10,7 @@ if (typeof busNum === `string`) {
     console.log(`*** INVALID BUS NUMBER ***`);
 } else if (busNum === 125) {
     startHour = 8
-    startMinute = 3
+    startMinute = 13
     interval = 8
 } else if (busNum === 136) {
     startHour = 7
@@ -17,12 +18,37 @@ if (typeof busNum === `string`) {
     interval = 6
 } else {
     console.log(`Bus no.${busNum} is not available.`);
-};
+} 
 
-console.log(busNum);
-console.log(startHour);
-console.log(startMinute);
-console.log(interval);
+function timeSchedule(inputTime) {
+    const [inputHour, inputMinute] = inputTime.split(`:`).map(Number);
+    const inputTimeInMinutes = inputHour * 60 + inputMinute;
+    let startTimeInMinutes = startHour * 60 + startMinute;
+
+    const minutesSinceStart = inputTimeInMinutes - startTimeInMinutes;
+    const minutesLeft = interval - (minutesSinceStart % interval);
+
+    let nextTimeInMinutes = inputTimeInMinutes + minutesLeft;
+
+    if (inputTimeInMinutes < startTimeInMinutes) {
+        console.log(`Here is the timetable for bus no.${busNum}:`);
+        for (let i = 0; i < 10; i++) {
+            const earlyHour = Math.floor(startTimeInMinutes / 60);
+            const earlyMinute = startTimeInMinutes % 60;
+            console.log(`${i + 1}. ${earlyHour}:${earlyMinute < 10 ? `0${earlyMinute}` : earlyMinute}`);
+            startTimeInMinutes += 8;
+        }
+    } else {
+        console.log(`The next 10 schedules for bus no.${busNum} are:`);
+
+        for (let i = 0; i < 10; i++) {
+            const scheduleHour = Math.floor(nextTimeInMinutes / 60);
+            const scheduleMinute = nextTimeInMinutes % 60;
+            console.log(`${i + 1}. ${scheduleHour}:${scheduleMinute < 10 ? `0${scheduleMinute}` : scheduleMinute}`);
+            nextTimeInMinutes += interval;
+        }
+    }
+}
 
 function busSchedule(startHour, startMinute, interval, inputTime) {
     const startTimeInMinute = startHour * 60 + startMinute;
@@ -31,11 +57,8 @@ function busSchedule(startHour, startMinute, interval, inputTime) {
 
     if (inputTimeInMinute < startTimeInMinute) {
         console.log(`No buses yet`);
-        if (startMinute < 10) {
-            console.log(`The first bus no.${busNum} will start at ${startHour}:0${startMinute}`);
-        } else {
-            console.log(`The first bus no.${busNum} will start at ${startHour}:${startMinute}`);
-        }
+        console.log(`The first bus no.${busNum} starts at ${startHour}:${startMinute < 10 ? `0${startMinute}` : startMinute}.`);
+        return;
     }
 
     const minuteSinceStart = inputTimeInMinute - startTimeInMinute;
@@ -48,8 +71,10 @@ const minutesLeft = busSchedule(startHour, startMinute, interval, inputTime);
 
 if (typeof minutesLeft === `string`) {
     console.log(minutesLeft);
+} else if (tt === `time`) {
+    timeSchedule(inputTime);
 } else if (minutesLeft === 0) {
     console.log(`Bus no.${busNum} is arriving now!`);
 } else {
-    console.log(`Bus no.${busNum} will arrive in ${minutesLeft} minute(s)`);
+    console.log(`The next bus no.${busNum} will be in ${minutesLeft} minute(s).`);
 }
