458533d3caf37034a8c23ba10eaffa0f852ffbaa
alanmakareem <fadhlanalan@proton.me>
2025-01-19 15:02:01 +0100

    Bus Schedule Timetable

---
+++ busSchedule.js
@@ -0,0 +1,45 @@
+const busNum = 125;
+const inputTime = `9:12`;
+
+let startHour = null;
+let startMinute = null;
+let interval = null;
+
+if (typeof busNum === `string`) {
+    console.log(`*** INVALID BUS NUMBER ***`);
+} else if (busNum === 125) {
+    startHour = 8
+    startMinute = 3
+    interval = 8
+} else if (busNum === 136) {
+    startHour = 7
+    startMinute = 56
+    interval = 6
+} else {
+    console.log(`Bus no.${busNum} is not available.`);
+};
+
+console.log(busNum);
+console.log(startHour);
+console.log(startMinute);
+console.log(interval);
+
+function busSchedule(startHour, startMinute, interval, inputTime) {
+    const startTimeInMinute = startHour * 60 + startMinute;
+    const [inputHour, inputMinute] = inputTime.split(`:`).map(Number);
+    const inputTimeInMinute = inputHour * 60 + inputMinute;
+
+    if (inputTimeInMinute < startTimeInMinute) {
+        console.log(`No buses yet`);
+        if (startMinute < 10) {
+            console.log(`The first bus no.${busNum} will start at ${startHour}:0${startMinute}`);
+        } else {
+            console.log(`The first bus no.${busNum} will start at ${startHour}:${startMinute}`);
+        }
+    }
+
+    const minuteSinceStart = inputTimeInMinute - startTimeInMinute;
+    const minutesLeft = interval - (minuteSinceStart % interval);
+
+    return minutesLeft === interval ? 0 : minutesLeft;
+}
